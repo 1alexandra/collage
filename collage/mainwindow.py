937d0bf4517bf315487de470f68cfbad475078c1
1alexandra <a.v.zhuravskaya@gmail.com>
2020-03-26 12:52:31 +0300

    add row, col to arguments of Application.create_*; change canvas buttons sticky and weight

--- collage/mainwindow.py
+++ collage/mainwindow.py
@@ -20,17 +20,17 @@ class Application(tk.Frame):
         left_frame = tk.LabelFrame(self, text='Menu', padx=10, pady=10)
         grid_frame(left_frame, [0, 1, 2], [0], 0, 0, 'nw')
         right_frame = tk.Frame(self, padx=10, pady=10)
-        grid_frame(right_frame, [1], [1], 0, 1, 'news')
+        grid_frame(right_frame, [0], [0], 0, 1, 'news')
 
-        self.create_buttons(left_frame)
-        self.create_entries(left_frame)
-        self.create_add_text_button(left_frame)
-        self.create_canvas_frame(right_frame)
+        self.create_buttons(left_frame, 0, 0)
+        self.create_entries(left_frame, 1, 0)
+        self.create_add_text_button(left_frame, 2, 0)
+        self.create_canvas_frame(right_frame, 0, 0)
 
-    def create_buttons(self, frame):
+    def create_buttons(self, frame, row, col):
         # TODO: add icons
         buttons_frame = tk.Frame(frame, bd=10)
-        grid_frame(buttons_frame, [0, 1, 2], [0, 1], 0, 0, 'news')
+        grid_frame(buttons_frame, [0, 1, 2], [0, 1], row, col, 'news')
         commands = {
             'Undo': self.undo_command,
             'Redo': self.redo_command,
@@ -43,10 +43,10 @@ class Application(tk.Frame):
             button = tk.Button(buttons_frame, text=text, command=command, width=10)
             button.grid(row=i // 2, column=i % 2, sticky='new')
 
-    def create_entries(self, frame):
+    def create_entries(self, frame, row, col):
         # TODO: add validation functions to entries
         entries_frame = tk.Frame(frame, bd=10)
-        grid_frame(entries_frame, [0, 1, 2, 3], [0, 1], 1, 0, 'news')
+        grid_frame(entries_frame, [0, 1, 2, 3], [0, 1], row, col, 'news')
         variables = {
             'Width in pixels': self.collage_width,
             'Height in pixels': self.collage_height,
@@ -55,32 +55,35 @@ class Application(tk.Frame):
         }
         for i, (text, variable) in enumerate(variables.items()):
             label = tk.Label(entries_frame, text=text, padx=5)
-            entry = tk.Entry(entries_frame, width=10, textvariable=variable)
+            entry = tk.Entry(entries_frame, textvariable=variable, width=10)
             label.grid(row=i, column=0, sticky='e')
             entry.grid(row=i, column=1, sticky='w')
-
         button = tk.Button(entries_frame, text="Change parameters", command=self.change_canvas_parameters)
         button.grid(row=len(variables), column=0, columnspan=2, sticky='new')
 
-    def create_add_text_button(self, frame):
+    def create_add_text_button(self, frame, row, col):
         text_button_frame = tk.Frame(frame, bd=10)
-        grid_frame(text_button_frame, [0], [0], 2, 0, 'news')
+        grid_frame(text_button_frame, [0], [0], row, col, 'news')
         self.text_button = tk.Button(text_button_frame, text="Add text...", command=self.open_text_window, padx=5, pady=5)
         self.text_button.grid()
 
-    def create_canvas_frame(self, frame):
+    def create_canvas_frame(self, frame, row, col):
+        # TODO: add two scrolling bars to canvas_frame
         # TODO: bind commands
-        self.add_left = tk.Button(frame, text='+')
-        self.add_right = tk.Button(frame, text='+')
-        self.add_up = tk.Button(frame, text='+')
-        self.add_down = tk.Button(frame, text='+')
+        canvas_frame = tk.Frame(frame, bd=10)
+        grid_frame(canvas_frame, [0, 2], [0, 2], row, col, 'news')
 
-        self.add_left.grid(row=1, column=0, sticky='nws')
-        self.add_right.grid(row=1, column=2, sticky='nes')
+        self.add_left = tk.Button(canvas_frame, text='+')
+        self.add_right = tk.Button(canvas_frame, text='+')
+        self.add_up = tk.Button(canvas_frame, text='+')
+        self.add_down = tk.Button(canvas_frame, text='+')
+
+        self.add_left.grid(row=1, column=0, sticky='nes')
+        self.add_right.grid(row=1, column=2, sticky='nws')
         self.add_up.grid(row=0, column=1, sticky='ews')
         self.add_down.grid(row=2, column=1, sticky='new')
 
-        self.collage = tk.Canvas(frame, bg='white')
+        self.collage = tk.Canvas(canvas_frame, bg='white')
         self.collage.grid(row=1, column=1)
         self.change_canvas_parameters()
 
