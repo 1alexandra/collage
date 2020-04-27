3d102524af37b8cd055de2b99d05ddbe4268a0bb
1alexandra <a.v.zhuravskaya@gmail.com>
2020-04-27 16:32:30 +0300

    updated path to modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 # import os
 import sys
-sys.path.insert(0, r'C:\Users\Александра\PROGRAM\Collage')
-
+sys.path.insert(0, '../..')
 
 # -- Project information -----------------------------------------------------
 
