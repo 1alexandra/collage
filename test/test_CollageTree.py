358688c7ce030f93127ff6040e774bc4e4709d30
krchmark <onymoth@gmail.com>
2020-05-08 05:38:46 +0400

    add new test for CollageTree

--- test/test_CollageTree.py
+++ test/test_CollageTree.py
@@ -1,11 +1,9 @@
-from src.CollageTree import CollageRoot, ResizableLeaf
+from src.CollageTree import ResizableLeaf
 from src.Collage import Collage
 from src.CollageImage import safe_open_image
-from src.scroll import ScrolledFrame
 
 import pytest
 import os
-import tkinter as tk
 
 
 @pytest.fixture
@@ -15,7 +13,8 @@ def filename():
 
 @pytest.fixture
 def collage():
-    collage = Collage(0, 1, 1, 1, None, [], {'width': 30, 'height': 30})
+    collage = Collage(0, 1, 1, 1, None, [], {'width': 99, 'height': 99})
+    collage.grid(row=0, column=0)
     return collage
 
 
@@ -30,8 +29,8 @@ def collage_image(filename, collage):
 
 
 def test_init(collage_root):
-    assert collage_root.get_width() == 30
-    assert collage_root.get_height() == 30
+    assert collage_root.get_width() == 99
+    assert collage_root.get_height() == 99
 
 
 def count_vertices(root):
@@ -61,6 +60,25 @@ def test_vertices_count_add(collage_root, collage_image):
         leafs = count_leafs(collage_root)
         assert vertices == 2 * i
         assert leafs == i
+
+
+def check_window_sizes(tree):
+    if tree.get_left() is not None:
+        left = tree.get_left()
+        right = tree.get_right()
+        assert abs(left.get_width() - right.get_width()) <= 1
+        assert abs(left.get_height() - right.get_height()) <= 1
+        check_window_sizes(left)
+        check_window_sizes(right)
+
+
+def test_windows_size(collage_root, collage_image):
+    sides = ['n', 'e', 'w', 's']
+    for i in range(1, 2 * len(sides) + 1):
+        cur_side = sides[i % len(sides)]
+        collage_root.add_image(image=collage_image, where=cur_side)
+    collage_root.get_tk_object().update()
+    check_window_sizes(collage_root.get_left())
 
 
 def test_vertices_count_remove(collage_root, collage_image):
