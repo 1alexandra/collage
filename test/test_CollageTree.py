8c9c53bbc19508c5e368fdd221cfd4dbe05edf2d
krchmark <onymoth@gmail.com>
2020-05-08 06:00:19 +0400

    add save_collage test

--- test/test_CollageTree.py
+++ test/test_CollageTree.py
@@ -1,9 +1,14 @@
 from src.CollageTree import ResizableLeaf
 from src.Collage import Collage
 from src.CollageImage import safe_open_image
+from PIL import Image
+from PIL import ImageChops
 
 import pytest
 import os
+
+
+BORDER_WIDTH = 2
 
 
 @pytest.fixture
@@ -12,8 +17,13 @@ def filename():
 
 
 @pytest.fixture
+def collage_filename():
+    return os.path.join('test', 'files', 'collage.png')
+
+
+@pytest.fixture
 def collage():
-    collage = Collage(0, 1, 1, 1, None, [], {'width': 99, 'height': 99})
+    collage = Collage(2, BORDER_WIDTH, 1, 1, None, [], {'width': 99, 'height': 99})
     collage.grid(row=0, column=0)
     return collage
 
@@ -26,6 +36,11 @@ def collage_root(collage):
 @pytest.fixture
 def collage_image(filename, collage):
     return safe_open_image(filename, collage.get_corners())
+
+
+@pytest.fixture
+def collage_images(filename, collage):
+    return [safe_open_image(filename, collage.get_corners()) for _ in range(12)]
 
 
 def test_init(collage_root):
@@ -68,17 +83,35 @@ def check_window_sizes(tree):
         right = tree.get_right()
         assert abs(left.get_width() - right.get_width()) <= 1
         assert abs(left.get_height() - right.get_height()) <= 1
+        total_width = left.get_width() + right.get_width() + BORDER_WIDTH
+        total_height = left.get_height() + right.get_height() + BORDER_WIDTH
+        assert total_width == tree.get_width() or total_height == tree.get_height()
         check_window_sizes(left)
         check_window_sizes(right)
 
 
-def test_windows_size(collage_root, collage_image):
+def test_windows_size(collage_root, collage_images):
     sides = ['n', 'e', 'w', 's']
-    for i in range(1, 2 * len(sides) + 1):
+    for i in range(1, 6):
         cur_side = sides[i % len(sides)]
-        collage_root.add_image(image=collage_image, where=cur_side)
+        collage_root.add_image(image=collage_images[i - 1], where=cur_side)
     collage_root.get_tk_object().update()
     check_window_sizes(collage_root.get_left())
+
+
+def test_save_collage(collage_root, collage_images, collage_filename):
+    sides = ['n', 'e', 'w', 's']
+    for i in range(1, 6):
+        cur_side = sides[i % len(sides)]
+        collage_root.add_image(image=collage_images[i - 1], where=cur_side)
+    collage_root.get_tk_object().update()
+    save_path = os.path.join('test', 'files', 'collage_new.png')
+
+    collage_root.save_collage(save_path)
+    true_collage = Image.open(collage_filename)
+    test_collage = Image.open(save_path)
+    diff = ImageChops.difference(true_collage, test_collage)
+    assert not diff.getbbox()
 
 
 def test_vertices_count_remove(collage_root, collage_image):
